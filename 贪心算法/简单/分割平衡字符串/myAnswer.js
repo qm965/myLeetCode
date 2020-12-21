d3c127837d23efe4762a984fd792141602abf74c
秦明 <xuefeng.lqm@raycloud.com>
2020-12-21 13:05:22 +0800

    分割平衡字符串

--- 贪心算法/简单/分割平衡字符串/myAnswer.js
+++ 贪心算法/简单/分割平衡字符串/myAnswer.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-
+    /**
+     * 利用栈的方法
+    */
+    var stack = [s[0]],max=0,index=1;
+    while(index<s.length){
+        if(stack[0]==s[index] || stack.length==0){
+            stack.unshift(s[index])
+        }else{
+            stack.shift()
+        }
+        stack.length ==0 && max++;
+        index += 1
+    }
+    console.log(max)
+    return max
 };
+
+balancedStringSplit("RLRRLLRLRL")
