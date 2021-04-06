7e80b754f3b0e4d2c5cd48c55c1d0c210733d187
秦明 <xuefeng.lqm@raycloud.com>
2021-04-06 09:50:55 +0800

    1

---
+++ 贪心算法/简单/种花问题/myAnswer.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count =0;
+        return false;
+    }
+    public static void main(String[] args) {
+        canPlaceFlowers([1,0,0,0],3);
+    }
+}
